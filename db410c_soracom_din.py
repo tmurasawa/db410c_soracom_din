f3035c4192fbc4d1b3c345cee022f63b2d010639
root <toru.murasawa@gmail.com>
2016-07-28 07:46:19 +0000

    add publish

--- db410c_soracom_din.py
+++ db410c_soracom_din.py
@@ -2,13 +2,27 @@
 import time
 
 from gpio_96boards import GPIO
+import paho.mqtt.client as mqtt
 
-cnt = 0
-
+# GPIO_A is D_A on Mezzanine
 GPIO_A = GPIO.gpio_id('GPIO_A')
 pins = (
     (GPIO_A, 'in'),
 )
+
+def on_connect(client, userdata, rc):
+  print("Connected with result code " + str(rc))
+  #if you use Scalenics WITHOUT SORACOM,comment out below..
+  #client.subscribe("<DEVICE_TOKEN>/<DEVICE_ID>/subscribe")
+  #if you use Scalenics WITH SORACOM,comment out below..
+  client.subscribe(topic)
+
+def on_disconnect(client, userdata, rc):
+  if rc != 0:
+     print("Unexpected disconnection.")
+
+def on_publish(client, userdata, mid):
+  print("publish: {0}".format(mid))
 
 
 def tilt(gpio):
@@ -25,6 +39,30 @@ def tilt(gpio):
 
 			if pin_current:
 				print "Push!"
+				print "Published topic:%s" %topic
+				# send ON
+				client.publish(topic, "v=1")
+				time.sleep(0.1)
+				# send OFF
+				client.publish(topic, "v=0")
+
+
+print "-- Get metadata from SORACOM..."
+
+
+import json
+import requests
+
+metadata=requests.get('http://metadata.soracom.io/v1/subscriber').json()
+#print metadata
+imsi=metadata["imsi"]
+print "imsi:%s" %imsi
+
+device_token=requests.get('http://metadata.soracom.io/v1/userdata').text
+print "device_token:%s" %device_token
+
+topic = device_token
+print "MQTT topic:%s" %topic
 
 
 if __name__ == '__main__':
@@ -34,5 +72,20 @@ if __name__ == '__main__':
         description='Blink LED on GPIO A (pin 23)')
     args = parser.parse_args()
 
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_disconnect = on_disconnect
+    #client.on_message = on_message
+
+		## if you use Scalenics WITHOUT SORACOM,comment out below..
+		#client.username_pw_set("<SCALENICS_ID>","<DEVICE_TOKEN>")
+		#client.connect("api.scalenics.io", 1883, 10)
+		## if you use Scalenics WITH SORACOM,comment out below..
+    client.connect("beam.soracom.io", 1883, 10)
+
     with GPIO(pins) as gpio:
+
         tilt(gpio)
+
+
